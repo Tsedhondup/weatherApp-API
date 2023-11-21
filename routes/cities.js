f9c05748e3f417fb63355c4f9907f02b263d0658
Tsering Dhondup <tyddhondup88@gmail.com>
2023-11-20 21:04:41 -0500

    API working

--- routes/cities.js
+++ routes/cities.js
@@ -24,51 +24,38 @@ router.get("/cities", (req, res, next) => {
 // POST THE VIDEO
 router.post("/cities", (req, res, next) => {
   // read file
+  let postCity;
   fs.readFile("./data/cities.json", (err, data) => {
     const cityArrayCopy = JSON.parse(data); // create a copy of existing data base
-    const postCity = { id: uuidv4(), city: req.body.city };
+    postCity = { id: uuidv4(), city: req.body.city };
     cityArrayCopy.push(postCity);
     // update data base
     fs.writeFile("./data/cities.json", JSON.stringify(cityArrayCopy), (err) => {
       console.log("cannot update the data base");
     });
+    res.send(postCity);
+    return;
   });
-  res.send(req.body);
-  return;
 });
 
 // // DELETE COMMENTS
-// router.delete("/cities/:id", (req, res, next) => {
-//   fs.readFile("./data/videos.json", (err, data) => {
-//     if (err) {
-//       res.send("Cannot delete comments");
-//       return;
-//     } else {
-//       const videoId = req.params.id; // get the video id
-//       const commentId = req.params.commentId; // get the comment id
-//       const videoDataCopy = JSON.parse(data); // create a shallow copy of video.json data
-//       // deleting comment
-//       videoDataCopy.forEach((videoObj) => {
-//         if (videoObj.id === videoId) {
-//           videoObj.comments = videoObj.comments.filter((comment) => {
-//             return comment.id !== commentId;
-//           });
-//         }
-//       });
-//       // updating data base
-//       fs.writeFile(
-//         "./data/videos.json",
-//         JSON.stringify(videoDataCopy),
-//         (err) => {
-//           if (err) {
-//             res.send("cannot delete comment!");
-//           }
-//         }
-//       );
-//       res.json({ id: commentId }); // sending deleted comment Id
-//       return;
-//     }
-//   });
-// });
+router.delete("/cities/:id", (req, res, next) => {
+  fs.readFile("./data/cities.json", (err, data) => {
+    const reqCityId = req.params.id;
+    const cityListCopy = JSON.parse(data);
+    const updatedCityList = cityListCopy.filter((city) => {
+      return city.id !== reqCityId;
+    });
+    fs.writeFile(
+      "./data/cities.json",
+      JSON.stringify(updatedCityList),
+      (err) => {
+        console.log("City added!");
+      }
+    );
+    res.send(updatedCityList);
+    return;
+  });
+});
 
 module.exports = router;
